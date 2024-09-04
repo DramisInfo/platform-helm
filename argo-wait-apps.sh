36b811793f7e1a199ff29b7089f3fa5078a9503a
Frederic Simard <frederic.simard@dramisinfo.com>
2024-09-03 22:37:31 -0400

    add prometheus

--- argo-wait-apps.sh
+++ argo-wait-apps.sh
@@ -6,6 +6,12 @@ INTERVAL=5  # Check every 5 seconds
 ELAPSED=0
 declare -A PREV_STATUS
 
+# ANSI color codes
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+ORANGE='\033[0;33m'
+NC='\033[0m' # No Color
+
 echo "Waiting for all applications to become healthy..."
 while [ $ELAPSED -lt $TIMEOUT ]; do
   CURRENT_STATUS=$(kubectl -n $NAMESPACE get applications -o jsonpath="{range .items[*]}{.metadata.name}{': '}{.status.health.status}{'\n'}{end}")
@@ -15,7 +21,13 @@ while [ $ELAPSED -lt $TIMEOUT ]; do
     APP_STATUS=$(echo "$line" | awk -F': ' '{print $2}')
     
     if [ "${PREV_STATUS[$APP_NAME]}" != "$APP_STATUS" ]; then
-      echo "Application $APP_NAME status changed: ${PREV_STATUS[$APP_NAME]} -> $APP_STATUS"
+      if [ "$APP_STATUS" == "Healthy" ]; then
+        echo -e "Application $APP_NAME status changed: ${PREV_STATUS[$APP_NAME]} -> ${GREEN}$APP_STATUS${NC}"
+      elif [ "$APP_STATUS" == "Progressing" ]; then
+        echo -e "Application $APP_NAME status changed: ${PREV_STATUS[$APP_NAME]} -> ${ORANGE}$APP_STATUS${NC}"
+      else
+        echo -e "Application $APP_NAME status changed: ${PREV_STATUS[$APP_NAME]} -> ${RED}$APP_STATUS${NC}"
+      fi
       PREV_STATUS[$APP_NAME]=$APP_STATUS
     fi
   done <<< "$CURRENT_STATUS"
@@ -23,7 +35,7 @@ while [ $ELAPSED -lt $TIMEOUT ]; do
   UNHEALTHY_APPS=$(echo "$CURRENT_STATUS" | grep -v "Healthy")
   
   if [ -z "$UNHEALTHY_APPS" ]; then
-    echo "All applications are healthy."
+    echo -e "${GREEN}All applications are healthy.${NC}"
     exit 0
   fi
   
@@ -31,5 +43,5 @@ while [ $ELAPSED -lt $TIMEOUT ]; do
   ELAPSED=$((ELAPSED + INTERVAL))
 done
 
-echo "Timeout reached. Some applications are still not healthy."
+echo -e "${RED}Timeout reached. Some applications are still not healthy.${NC}"
 exit 1
